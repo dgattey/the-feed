7e51574646d88f7986c3b6008029ccfa6dc66fa1
Dylan Gattey <dylan.gattey@gmail.com>
2024-12-22 14:45:55 -0600

    Initial Commit

---
+++ The Feed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Feed
+//
+//  Created by Dylan Gattey on 12/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Feed/The_FeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_FeedApp.swift
+//  The Feed
+//
+//  Created by Dylan Gattey on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct The_FeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
