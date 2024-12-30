52172836401d0ad214ae0c7efed1d2294e30d82f
Dylan Gattey <dylan.gattey@gmail.com>
2024-12-29 20:28:38 -0600

    (temp) turn off text block and location for now

--- The Feed/Models/Entry/GroupedEntries.swift
+++ The Feed/Models/Entry/GroupedEntries.swift
@@ -5,13 +5,17 @@
 //  Created by Dylan Gattey on 12/23/24.
 //
 
-enum GroupedEntriesCategory: String, CaseIterable, SearchableModel, IdentifiableModel {
+enum GroupedEntriesCategory: String, SearchableModel, IdentifiableModel {
     case all = "All"
     case book = "Books"
     case location = "Locations"
     case textBlock = "Text blocks"
     
     var id: String { rawValue }
+    
+    static var allCases: Array<GroupedEntriesCategory> = [
+        .book
+    ]
     
     func contains(searchText: String) -> Bool {
         return id.localizedCaseInsensitiveContains(searchText)
